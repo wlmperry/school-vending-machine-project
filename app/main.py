0bfcf01122cf5009fd3ba14e8133699d0261a372
Wesley Perry <wlmperry98@gmail.com>
2025-02-02 21:16:03 -0500

    feat(functions): add main and start_machine functions
    - Added 'main()' to control program execution.
    - Included script control block using 'if __name__ == "__main__"'.
    - Created initial stub for 'start_machine()'.
    - Added 'ingredient_stock' dictionary to track machine's ingredient reserves.
    - Included comments for clarity and a to-do list for future code implementation.

---
+++ app/main.py
@@ -0,0 +1,33 @@
+
+# Vending machine's ingredient reserves.
+ingredient_stock = {
+    "water": 300, #mililiters
+    "milk": 200, #mililiters
+    "coffee": 100 #grams
+}
+
+def start_machine():
+    # Vending machine's current on/off state.
+    machine_on = True
+
+    # <<< To-do: >>>
+    # When the machine is on do this.
+
+    # If user enters 'exit' command.
+
+    # If user enters 'report' command.
+
+    # If user enters 'help' command.
+
+    # Assume the user enters an integer & send to a function that
+    # will process the order.
+
+    # Catch invalid entries or unexpected input.
+    # <<< To-do: >>>
+
+def main():
+    start_machine()
+
+# Start the program when the script is run
+if __name__ == "__main__":
+    main()
